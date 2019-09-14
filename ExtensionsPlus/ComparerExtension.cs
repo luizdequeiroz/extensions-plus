bc672fe9f6e8d13f42f62890f10baef69cb1904f
luizdequeiroz <oluizdequeiroz@gmail.com>
2019-09-14 18:22:34 -0300

    commit inicial, criação do projeto e implementação do EnumExtension GetAttr e ToDescriptionString, e implementação do ComparerExtension com o Equals e a classe interna GenericComparer que implementa o IEqualityComparer (versão .net framework 4.0).

---
+++ ExtensionsPlus/ComparerExtension.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ExtensionsPlus
+{
+    public static class ComparerExtension
+    {
+        public static bool Equals<E>(this E x, E y)
+        {
+            var genericComparer = new GenericComparer<E>();
+            return genericComparer.Equals(x, y);
+        }
+
+        public class GenericComparer<E> : IEqualityComparer<E>
+        {
+            public bool Equals(E x, E y)
+            {
+                if (ReferenceEquals(x, y)) return true;
+                if (ReferenceEquals(x, null) || ReferenceEquals(y, null))
+                    return false;
+
+                var propertiesCount = x.GetType().GetProperties().Count();
+                var equalsCount = 0;
+
+                foreach (var propertyX in x.GetType().GetProperties())
+                    foreach (var propertyY in y.GetType().GetProperties())
+                        if (propertyX.Name == propertyY.Name)
+                            if (propertyX.GetValue(x, null)?.ToString() == propertyY.GetValue(y, null)?.ToString())
+                                equalsCount++;
+
+                return propertiesCount == equalsCount;
+            }
+
+            public int GetHashCode(E e)
+            {
+                if (ReferenceEquals(e, null)) return 0;
+
+                int hashCodeObject = 0;
+
+                foreach (var property in e.GetType().GetProperties())
+                {
+                    var value = property.GetValue(e, null);
+                    var hashCode = (value != null ? property.GetValue(e, null).GetHashCode() : 0);
+                    if (hashCodeObject == 0) hashCodeObject = hashCode;
+                    else hashCodeObject ^= hashCode;
+                }
+
+                return hashCodeObject;
+            }
+        }
+    }    
+}
---
+++ ExtensionsPlus/EnumExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace ExtensionsPlus
+{
+    public static class EnumExtension
+    {
+        public static T GetAttr<T>(this Enum @enum) where T : Attribute
+        {
+            var memb = @enum.GetType().GetMember(@enum.ToString());
+            var attr = memb[0].GetCustomAttributes(typeof(T), false);
+            return (T)attr[0];
+        }
+
+        public static string ToDescriptionString(this Enum @enum)
+        {
+            var attr = @enum.GetAttr<DescriptionAttribute>();
+            return (attr == null) ? @enum.ToString() : attr.Description;
+        }
+    }
+}
